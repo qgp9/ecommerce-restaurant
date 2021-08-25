e0771b2909bf9952703e5b84fdd9aaa05dd98eba
sunjus <sunju.shin1108@gmail.com>
2021-08-25 14:55:49 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+const navBar = document.querySelector(".nav");
+
+if (hamburger) {
+  hamburger.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
+
+//Fix Nav
+const navHeight = navBar.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+//Pop up
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide-popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide-popup");
+    }, 1000);
+  });
+}
