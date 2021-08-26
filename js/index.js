285ede2b2a7ab29e1e725b9da1aca1b97a8b1abd
sunjus <sunju.shin1108@gmail.com>
2021-08-26 18:34:17 +0300

    Added menu page

--- js/index.js
+++ js/index.js
@@ -19,6 +19,44 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
+/*
+//Scroll
+const links = [...document.querySelectorAll(".nav-link")];
+
+links.map((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const el = document.getElementById(id);
+    const navHeight = navigation.getBoundingClientRect().height;
+    const fix = navigation.classList.contains("fix");
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+
+    navList.classList.remove("open");
+  });
+});
+*/
+//Scroll reveal
+const scroll = ScrollReveal({
+  distance: "100px",
+  duration: 2500,
+  reset: true,
+});
+
+scroll.reveal(`.menu-link h2 `, {
+  origin: "right",
+  interval: 100,
+});
+
+scroll.reveal(`.contact p, .location, .hour, iframe`, {
+  origin: "top",
+  interval: 100,
+});
 
 //Pop up
 const popup = document.querySelector(".popup");
