32b2716f1ed787c04c9c257991dd748ea0904013
sunjus <sunju.shin1108@gmail.com>
2021-09-17 20:18:36 +0300

    Add language swticher

--- js/index.js
+++ js/index.js
@@ -1,28 +1,64 @@
-let multiLanguage = {
-  en: { id: 1, name: "yachaejeon -- 8", ingre: "carrot, zucchini, onion" },
-  fi: { id: 5, name: "finnish -- 9", ingre: "finnish" },
+var languages = {
+  en: {
+    name: "yachaejeon -- 8",
+    ingre: "carrot, zucchini, onion",
+  },
+  fi: {
+    name: "finnish -- 9",
+    ingre: "finnish",
+  },
 };
 
-window.onload = () => {
-  let enBtn = document.getElementById("enBtn");
-  let fiBtn = document.getElementById("fiBtn");
-
-  let setLanguage = (lang) => {
-    let changeNodeList = Array.prototype.slice.call(
-      document.querySelectorAll("[data-detect]")
+(function () {
+  var currentLanguage;
+  var isButtonBlocked = false;
+  var section = document.querySelector("li.menu-starter");
+  var updateLayout = function () {
+    isButtonBlocked = true;
+    currentLanguage = section.getAttribute("lang").toLowerCase();
+    section.classList.add(
+      currentLanguage === "fi" ? "section-anim-rtl" : "section-anim"
     );
 
-    //각 dataset 중 detect인 요소들을 찾아 변경하는 곳
-    changeNodeList.map((v) => {
-      v.textContent = multiLanguage[lang][v.dataset.detect];
+    if (!languages[currentLanguage]) {
+      console.warn(currentLanguage + ": this language is not supported.");
+      currentLanguage = "en";
+    }
+
+    var fields = section.querySelectorAll("[data-detect]");
+    fields.forEach(function (el) {
+      const type = el.getAttribute("data-detect");
+
+      if (!languages[currentLanguage][type]) {
+        console.warn("Error: Field with type '" + type + "' is not supported.");
+        return;
+      }
+
+      el.textContent = languages[currentLanguage][type];
     });
+    setTimeout(function () {
+      section.classList.remove("section-anim");
+      section.classList.remove("section-anim-rtl");
+      isButtonBlocked = false;
+    }, 500);
   };
 
-  enBtn.addEventListener("click", () => {
-    setLanguage(enBtn.dataset.lang);
-  });
+  var changeBtn = document.querySelector("button.change-language");
+  changeBtn.onclick = function () {
+    if (isButtonBlocked) {
+      return;
+    }
 
-  fiBtn.addEventListener("click", () => {
-    setLanguage(fiBtn.dataset.lang);
-  });
-};
+    if (currentLanguage === "en") {
+      section.setAttribute("lang", "fi");
+      section.style.direction = "rtl";
+      updateLayout();
+      return;
+    }
+    section.setAttribute("lang", "en");
+    section.style.direction = "ltr";
+    updateLayout();
+  };
+  section.style.display = "block";
+  updateLayout();
+})();
