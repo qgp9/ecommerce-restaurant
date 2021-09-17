c61b7f8523ba0d0257b6888075568ac418ba0318
sunjus <sunju.shin1108@gmail.com>
2021-09-17 21:37:44 +0300

    Add finnish menu

--- js/index.js
+++ js/index.js
@@ -1,64 +1,22 @@
-var languages = {
-  en: {
-    name: "yachaejeon -- 8",
-    ingre: "carrot, zucchini, onion",
-  },
-  fi: {
-    name: "finnish -- 9",
-    ingre: "finnish",
-  },
-};
+function myFunction() {
+  var en = document.getElementById("en");
+  var fi = document.getElementById("fi");
+  if (en.style.display === "block") {
+    en.style.display = "block";
+  } else {
+    en.style.display = "block";
+    fi.style.display = "none";
+  }
+}
 
-(function () {
-  var currentLanguage;
-  var isButtonBlocked = false;
-  var section = document.querySelector("li.menu-starter");
-  var updateLayout = function () {
-    isButtonBlocked = true;
-    currentLanguage = section.getAttribute("lang").toLowerCase();
-    section.classList.add(
-      currentLanguage === "fi" ? "section-anim-rtl" : "section-anim"
-    );
-
-    if (!languages[currentLanguage]) {
-      console.warn(currentLanguage + ": this language is not supported.");
-      currentLanguage = "en";
-    }
-
-    var fields = section.querySelectorAll("[data-detect]");
-    fields.forEach(function (el) {
-      const type = el.getAttribute("data-detect");
-
-      if (!languages[currentLanguage][type]) {
-        console.warn("Error: Field with type '" + type + "' is not supported.");
-        return;
-      }
-
-      el.textContent = languages[currentLanguage][type];
-    });
-    setTimeout(function () {
-      section.classList.remove("section-anim");
-      section.classList.remove("section-anim-rtl");
-      isButtonBlocked = false;
-    }, 500);
-  };
-
-  var changeBtn = document.querySelector("button.change-language");
-  changeBtn.onclick = function () {
-    if (isButtonBlocked) {
-      return;
-    }
-
-    if (currentLanguage === "en") {
-      section.setAttribute("lang", "fi");
-      section.style.direction = "rtl";
-      updateLayout();
-      return;
-    }
-    section.setAttribute("lang", "en");
-    section.style.direction = "ltr";
-    updateLayout();
-  };
-  section.style.display = "block";
-  updateLayout();
-})();
+function myFunctionfi() {
+  var en = document.getElementById("en");
+  var fi = document.getElementById("fi");
+  if (fi.style.display === "none") {
+    fi.style.display = "block";
+    en.style.display = "none";
+  } else {
+    fi.style.display = "none";
+    en.style.display = "block";
+  }
+}
