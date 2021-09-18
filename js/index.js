c295a76ccc16c5ea9c40c000176f7031cf4d47c9
sunjus <sunju.shin1108@gmail.com>
2021-09-18 15:32:01 +0300

    Add reservation backround

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
-function myFunction() {
-  var en = document.getElementById("en");
-  var fi = document.getElementById("fi");
-  if (en.style.display === "block") {
-    en.style.display = "block";
-  } else {
-    en.style.display = "block";
-    fi.style.display = "none";
-  }
+function setLanguageEn() {
+  const en = document.getElementById("en");
+  const fi = document.getElementById("fi");
+  en.style.display = "block";
+  fi.style.display = "none";
 }
 
-function myFunctionfi() {
-  var en = document.getElementById("en");
-  var fi = document.getElementById("fi");
-  if (fi.style.display === "none") {
-    fi.style.display = "block";
-    en.style.display = "none";
-  } else {
-    fi.style.display = "none";
-    en.style.display = "block";
-  }
+function setLanguageFi() {
+  const en = document.getElementById("en");
+  const fi = document.getElementById("fi");
+  fi.style.display = "block";
+  en.style.display = "none";
 }
