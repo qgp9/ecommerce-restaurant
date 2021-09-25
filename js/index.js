a19107a768f265a005cefc8f38cbee6a6c71cc13
sunjus <sunju.shin1108@gmail.com>
2021-09-25 18:15:47 +0300

    Add loader

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+function init() {
+  const loader = document.querySelector(".loader");
+  const main = document.querySelector(".main");
+
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = "none";
+
+    main.style.display = "block";
+    setTimeout(() => (main.style.opacity = 1), 50);
+  }, 4000);
+}
+
+init();
+
 function setLanguageEn() {
   const en = document.getElementById("en");
   const fi = document.getElementById("fi");
